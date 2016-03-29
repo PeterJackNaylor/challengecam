c09f95a9b601a1c1953b321a0fdcc33392f960a1
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-03-29 12:13:16 +0200

    good

--- cluster/bash_files/tests/checking_training_set.py
+++ cluster/bash_files/tests/checking_training_set.py
@@ -102,6 +102,5 @@ if __name__ ==  "__main__":
 					tttttt = 1
 			except:
 				of.write("Problems with "+slide+"\n")
->>>>>>> 91f5b123b92d12abbe569cc38ba6c5cff5eb2646
 	of.write("Their is %d lines \n" % n_lines)
 	of.write("Their is %d ones \n" % n_ones)
