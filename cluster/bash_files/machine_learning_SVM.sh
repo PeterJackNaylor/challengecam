dd233068159a78ae6f4da292a6a3b3e7d7539f13
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-04-01 11:55:01 +0200

    SVM nearly there

--- cluster/bash_files/machine_learning_SVM.sh
+++ cluster/bash_files/machine_learning_SVM.sh
@@ -21,7 +21,7 @@ OUTPUT=/share/data40T_v2/challengecam_results/SVM/
 spe_tag=__
 PYTHON_FILE=/share/data40T/pnaylor/Cam16/scripts/challengecam/cluster/machine_learning_SVM.py
 
-KGROUPS=50
+KGROUPS=30
 KSAMPLES=1
 KERNEL=rbf
 
