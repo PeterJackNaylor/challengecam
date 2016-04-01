73799f87b7cab85abf4867ce4e90903dc1c9694b
PeterJackNaylor <peter.naylor@mines-paristech.fr>
2016-04-01 11:15:17 +0200

    rbf svm

--- cluster/bash_files/machine_learning_SVM.sh
+++ cluster/bash_files/machine_learning_SVM.sh
@@ -17,12 +17,17 @@
 
 CAM16=/share/data40T_v2/challengecam_results/train/
 KFOLD=/share/data40T_v2/challengecam_results/training/kfold.txt
-OUTPUT=/share/data40T_v2/challengecam_results/training/
+OUTPUT=/share/data40T_v2/challengecam_results/SVM/
 spe_tag=__
-PYTHON_FILE=/share/data40T/pnaylor/Cam16/scripts/challengecam/cluster/machine_learning.py
+PYTHON_FILE=/share/data40T/pnaylor/Cam16/scripts/challengecam/cluster/machine_learning_SVM.py
+
+KGROUPS=30
+KSAMPLES=2
+KERNEL=rbf
+
 source $HOME/.bash_profile
 
-FILE=/share/data40T_v2/challengecam_results/settings_for_machine_learning.txt # fichier csv (delimiter=' ') où la premiere colonne est la valeur de $PBS_ARRAYID, la seconde est le nom du programme, et les autres les différents paramètres à faire passer au code python
+FILE=/share/data40T_v2/challengecam_results/settings_for_machine_learning_SVM.txt # fichier csv (delimiter=' ') où la premiere colonne est la valeur de $PBS_ARRAYID, la seconde est le nom du programme, et les autres les différents paramètres à faire passer au code python
 FIELD1=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f2) # la partie gauche est pour chopper la ligne numéro $PBS_ARRAYID
 FIELD2=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f3) # la partie droite est pour chopper la valeur qui est dans la colonne voulue 
 FIELD3=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f4) # sachant que le séparateur est l'espace
@@ -30,5 +35,4 @@ FIELD4=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f5)
 FIELD5=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f6)
 FIELD6=$(grep "$spe_tag$SGE_TASK_ID$spe_tag " $FILE | cut -d' ' -f7)
 
-
-python $PYTHON_FILE --source $CAM16 --kfold_file $KFOLD --fold $FIELD1 --n_samples $FIELD2 --version $FIELD3 --n_tree $FIELD4 --m_try $FIELD5 --bootstrap $FIELD6 --save 1 --output $OUTPUT --n_jobs 1
+python $PYTHON_FILE --source $CAM16 --kfold_file $KFOLD --fold $FIELD1 --n_samples $FIELD2 --version $FIELD3 --norm1 0 --penalty $FIELD4 --save 1  --output $OUTPUT  --kmean_k $KGROUPS --kmean_n $KSAMPLES --kernel $KERNEL --gamma $FIELD5
